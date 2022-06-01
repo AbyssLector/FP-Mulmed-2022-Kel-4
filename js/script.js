6a7ae2d93dd1fded2ac5f36547013a0b8c714ab4
Nadine Haninta <nadinehaninta@gmail.com>
2022-06-02 00:01:23 +0700

    Update Int + FE #1

--- js/script.js
+++
@@ -1,9 +0,0 @@
-// const tesseract = require('node-tesseract-ocr')
-const tesseract = require('tesseract.js');
-
-tesseract.recognize("js/tes2.jpeg", "eng")
-    .then(result => {
-        console.log(result.data.text)
-    }).catch(err => {
-        console.log(err.message)
-    });
--- js/text-to-speech.js
+++ js/text-to-speech.js
@@ -1,4 +1,17 @@
-var txtInput = document.querySelector('#txtInput');
+const xhttp = new XMLHttpRequest();
+xhttp.onload = function() {
+    var test = JSON.parse(this.responseText);
+    // alert(test.msg)
+    document.getElementById("demo").innerHTML = test.msg;
+}
+xhttp.open("GET", "http://localhost:3000");
+xhttp.send();
+
+function updateInput(test){
+    document.getElementById("txtInput").innerHTML = document.getElementById("demo").innerHTML;
+}
+
+var txtInput = document.querySelector('#demo');
 var voiceList = document.querySelector('#voiceList');
 var btnSpeak = document.querySelector('#btnSpeak');
 var synth = window.speechSynthesis;
@@ -9,8 +22,9 @@ if (speechSynthesis !== undefined) {
     speechSynthesis.onvoiceschanged = PopulateVoices;
 }
 
-btnSpeak.addEventListener('click', () => {
-    var toSpeak = new SpeechSynthesisUtterance(txtInput.value);
+function autoClick(){
+    // document.getElementById("speak-btn").click();
+    var toSpeak = new SpeechSynthesisUtterance(txtInput.innerHTML);
     var selectedVoiceName = voiceList.selectedOptions[0].getAttribute('data-name');
     voices.forEach((voice) => {
         if (voice.name === selectedVoiceName) {
@@ -18,7 +32,21 @@ btnSpeak.addEventListener('click', () => {
         }
     });
     synth.speak(toSpeak);
-});
+}
+// btnSpeak.addEventListener('click', () => {
+// function myFunction(){
+//     // document.getElementById("#demo").click();
+//     var toSpeak = new SpeechSynthesisUtterance(txtInput.innerHTML);
+//     var selectedVoiceName = voiceList.selectedOptions[0].getAttribute('data-name');
+//     voices.forEach((voice) => {
+//         if (voice.name === selectedVoiceName) {
+//             toSpeak.voice = voice;
+//         }
+//     });
+//     synth.speak(toSpeak);
+//     // alert("awoga")
+// }
+// });
 
 function PopulateVoices() {
     voices = synth.getVoices();
